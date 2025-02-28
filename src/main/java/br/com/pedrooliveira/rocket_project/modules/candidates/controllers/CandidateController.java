1945ad3a58b2a7e96f25c3a48e2c0a2d4f7126be
Pedro Oliveira <pedroaolive@gmail.com>
2025-02-28 15:46:08 -0300

    change: add try catch block in create method controller

--- src/main/java/br/com/pedrooliveira/rocket_project/modules/candidates/controllers/CandidateController.java
+++ src/main/java/br/com/pedrooliveira/rocket_project/modules/candidates/controllers/CandidateController.java
@@ -1,9 +1,12 @@
 package br.com.pedrooliveira.rocket_project.modules.candidates.controllers;
 
+import br.com.pedrooliveira.rocket_project.exceptions.candidate.UserFoundException;
 import br.com.pedrooliveira.rocket_project.modules.candidates.entities.Candidate;
 import br.com.pedrooliveira.rocket_project.modules.candidates.repositories.CandidateRepository;
+import br.com.pedrooliveira.rocket_project.modules.candidates.useCases.CreateCandidateUseCase;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,10 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class CandidateController {
 
     @Autowired
-    private CandidateRepository repository;
+    private CreateCandidateUseCase createCandidateUseCase;
 
     @PostMapping("/add")
-    public Candidate create(@Valid @RequestBody Candidate candidate) {
-        return this.repository.save(candidate);
+    public ResponseEntity<Object> create(@Valid @RequestBody Candidate candidate) {
+        try {
+            Candidate result = this.createCandidateUseCase.execute(candidate);
+
+            return ResponseEntity.ok().body(result);
+        } catch (Exception ex) {
+            return ResponseEntity.badRequest().body(ex.getMessage());
+        }
     }
 }
