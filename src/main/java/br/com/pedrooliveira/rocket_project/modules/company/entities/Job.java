0c2e9aabf10abfd68be6edc2d9970b7449412eaa
Pedro Oliveira <pedroaolive@gmail.com>
2025-02-28 17:00:12 -0300

    config: create Job entity

--- src/main/java/br/com/pedrooliveira/rocket_project/modules/company/entities/Job.java
+++ src/main/java/br/com/pedrooliveira/rocket_project/modules/company/entities/Job.java
@@ -1,4 +1,43 @@
 package br.com.pedrooliveira.rocket_project.modules.company.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "job")
+@Data
 public class Job {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, length = 254)
+    private String name;
+
+    @Column(nullable = false, length = 254)
+    private String description;
+
+    @Column(nullable = false, length = 254)
+    private String level;
+
+    @Column(nullable = false, length = 254)
+    private String benefits;
+
+    @Column(nullable = false, length = 254)
+    private String requirements;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @ManyToOne()
+    @JoinColumn(name = "company_id", insertable = false, updatable = false)
+    private Company company;
+
+    @Column(name = "company_id")
+    private UUID companyId;
 }
