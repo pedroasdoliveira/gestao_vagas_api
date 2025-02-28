0e79c32e7bc0a9b561c3ef4cdc7c515447a90467
Pedro Oliveira <pedroaolive@gmail.com>
2025-02-28 20:47:09 -0300

    config: add Security Config

---
+++ src/main/java/br/com/pedrooliveira/rocket_project/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package br.com.pedrooliveira.rocket_project.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth
+                            .requestMatchers("/candidate", "/candidate/add").permitAll()
+                            .requestMatchers("/company", "/company/add").permitAll();
+                    auth.anyRequest().authenticated();
+                });
+
+        return http.build();
+    }
+}
