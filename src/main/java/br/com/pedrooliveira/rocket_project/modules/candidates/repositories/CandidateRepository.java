3fd8b2938cba30bc7e91a1defbf92b74b5cd1abd
Pedro Oliveira <pedroaolive@gmail.com>
2025-02-28 15:48:15 -0300

    refactor: add findByUsernameOrEmail method in CandidateRepository

--- src/main/java/br/com/pedrooliveira/rocket_project/modules/candidates/repositories/CandidateRepository.java
+++ src/main/java/br/com/pedrooliveira/rocket_project/modules/candidates/repositories/CandidateRepository.java
@@ -3,9 +3,10 @@ package br.com.pedrooliveira.rocket_project.modules.candidates.repositories;
 import br.com.pedrooliveira.rocket_project.modules.candidates.entities.Candidate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CandidateRepository extends JpaRepository<Candidate, UUID> {
 
-    public Candidate findByEmail(String email);
+    public Optional<Candidate> findByUsernameOrEmail(String username, String email);
 }
