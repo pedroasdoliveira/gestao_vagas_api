ded4053e4915a84dde6f94eed54ecb65f9dcb37f
Pedro Oliveira <pedroaolive@gmail.com>
2025-03-06 17:30:13 -0300

    feat: add password encoder when create a candidate

--- src/main/java/br/com/pedrooliveira/rocket_project/modules/candidates/useCases/CreateCandidateUseCase.java
+++ src/main/java/br/com/pedrooliveira/rocket_project/modules/candidates/useCases/CreateCandidateUseCase.java
@@ -4,6 +4,7 @@ import br.com.pedrooliveira.rocket_project.exceptions.candidate.UserFoundExcepti
 import br.com.pedrooliveira.rocket_project.modules.candidates.entities.Candidate;
 import br.com.pedrooliveira.rocket_project.modules.candidates.repositories.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,12 +13,18 @@ public class CreateCandidateUseCase {
     @Autowired
     private CandidateRepository repository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public Candidate execute(Candidate candidate) {
         this.repository
                 .findByUsernameOrEmail(candidate.getUsername(), candidate.getEmail())
                 .ifPresent((user) -> {
                     throw new UserFoundException("Usuário já existe na base de dados!");
                 });
+
+        final String password = passwordEncoder.encode(candidate.getPassword());
+        candidate.setPassword(password);
 
         return this.repository.save(candidate);
     }
